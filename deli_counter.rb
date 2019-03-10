607104890b13b90f77d71478eff4e0615bb83285
Pete A <peter.ayresmusic@gmail.com>
2019-03-10 21:46:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,10 @@ def line(katz_deli)
   if katz_deli.size == 0
     return "The line is currently empty."
   else
+     line =  "The line is currently:"
     katz_deli.each_with_index do |name, index|
-        return "The line is currently:#{index+1}")
-      end
-    end 
+         line += "#{index+1}. #{name}"
+    end
+         return line 
+  end 
+end 
